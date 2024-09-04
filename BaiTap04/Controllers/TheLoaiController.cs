69f725431d9227c5e3d67b262d13d08734d8b3eb
namborao <tranhoainam1806@gmail.com>
2024-09-04 14:30:16 +0700

    bai 04

---
+++ BaiTap04/Controllers/TheLoaiController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap04.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Edit(int id)
+        {
+            return Ok("Id: " + id);
+        }
+        public IActionResult Detail(int id, string ten)
+        {
+            //return Content("Id: " + id +"; ten: "+ ten);
+            return Content(string.Format("Id: {0}; ten: {1}", id, ten));
+        }
+        public IActionResult Show(List<string> categories)
+        {
+            string content = "danh sách thể loại: ";
+            foreach (var category in categories)
+            {
+                content = content + " " + category + ", ";
+            }
+            return Content(content);
+        }
+    }
+}
