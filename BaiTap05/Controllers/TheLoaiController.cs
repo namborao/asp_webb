0470409dfb5c8451a183551cc2d9e765f97cf023
namborao <tranhoainam1806@gmail.com>
2024-09-04 14:58:13 +0700

    bai 05

---
+++ BaiTap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,26 @@
+﻿using BaiTap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngày 1";
+            ViewBag.Thang = "thang 1";
+            ViewData["Nam"] = "Nam 2020";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Trinh tham"
+            };
+
+            return View(theloai);
+        }
+    }
+}
