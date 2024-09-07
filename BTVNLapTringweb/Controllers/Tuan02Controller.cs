7e61aeb3a996c5f666da5bee3df6866673b0d9be
namborao <tranhoainam1806@gmail.com>
2024-09-07 12:31:33 +0700

    btvn 02

---
+++ BTVNLapTringweb/Controllers/Tuan02Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha02.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.HoTen = "Tran Hoai Nam";
+            ViewBag.MSSV = "1822040472";
+            ViewBag.Nam = 2024;
+
+            return View();
+        }
+    }
+}
