c0808876917636a3715f070af31c49b62dbb5009
namborao <tranhoainam1806@gmail.com>
2024-09-07 12:40:30 +0700

    BTVN 03

---
+++ BTVNLapTrinhWeb03/Controllers/MayTinhController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha03.Controllers
+{
+    public class MayTinhController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MayTinh(int a = 0, int b = 0, string pheptinh = "")
+        {
+            int ketqua = 0;
+            string message = "";
+
+            if (pheptinh == "cong")
+            {
+                ketqua = a + b;
+            }
+            else if (pheptinh == "tru")
+            {
+                ketqua = a - b;
+            }
+            else if (pheptinh == "nhan")
+            {
+                ketqua = a * b;
+            }
+            else if (pheptinh == "chia")
+            {
+                if (b != 0)
+                {
+                    ketqua = a / b;
+                }
+                else
+                {
+                    message = "Không thể chia cho 0";
+                }
+            }
+            else
+            {
+                message = "Phép tính không hợp lệ";
+            }
+            ViewBag.KetQua = (message == "") ? $"Kết quả của {pheptinh} là: {ketqua}" : message;
+            return View();
+        }
+
+    }
+}
